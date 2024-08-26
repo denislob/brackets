e20d869e14a466db6cfa062eb48bfe2c687a0c8d
den <you@example.com>
2024-08-26 13:44:22 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pile = [];
+  let bracketsList = [];
+		
+  for(let sym = 0; sym < bracketsConfig.length; sym++) {
+		bracketsList[bracketsConfig[sym][0]] = bracketsConfig[sym][1];
+	}
+
+  for(let el = 0; el < str.length; el++) {
+		if(str[el] == pile[pile.length - 1]) {
+			pile.pop();
+		} else {
+			if(bracketsList[str[el]] != undefined) {
+                pile.push(bracketsList[str[el]]);
+      }     else {
+                return false;
+      }
+		}
+	}
+    return pile.length == 0;
 }
