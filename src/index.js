707ac0ad5c09348f2cb60bfe925dc6339db7c5a1
den <you@example.com>
2025-09-02 14:07:11 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const bracketsMap = {};
+  for (let i = 0; i < bracketsConfig.length; i += 1) {
+    const openBracket = bracketsConfig[i][0];
+    const closeBracket = bracketsConfig[i][1];
+    bracketsMap[openBracket] = closeBracket;
+  }
+
+  for (let el = 0; el < str.length; el += 1) {
+    const sym = str[el];
+    const lastSymInStack = stack[stack.length - 1];
+    if (lastSymInStack && sym === bracketsMap[lastSymInStack]) {
+      stack.pop();
+    } else if (bracketsMap[sym] !== undefined) {
+      stack.push(sym);
+    } else {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
 };
